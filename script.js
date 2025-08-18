3bed41d06edddcfaf1584c04e5f19518fa5cc8fb
Rocha1972 <gatheriano@gmail.com>
2025-08-18 17:52:21 -0300

    imagens e infos de Rocker

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     //se tiver light mode, adicione a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/rocker-light.png")
     img.setAttribute(
       "alt",
       "Foto de Mayk Brit usando óculos escuro, sorrindo, com jaqueta preta em fundo degrade, roxo e azul"
     )
   } else {
     //set sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/rocker.png")
     img.setAttribute(
       "alt",
       "Foto de Mayk Brito usando óculos, sorrindo, com camisa preta em fundo amarelo"
